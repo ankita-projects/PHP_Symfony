60de6ecf48e3b233739c45de7497451090358cff
Ankita Bhatnagar <ankita.bhatnagar@outlook.com>
2021-05-02 09:08:20 +0300

    creating recipie backend first initial implementation

--- src/Controller/HomeScreenController.php
+++ src/Controller/HomeScreenController.php
@@ -1,19 +1,45 @@
 <?php
 
-
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeScreenController extends AbstractController
 {
+
     /**
-     * @Route("/home", methods={"POST","GET"})
+     * @Route("/recipies",methods={"GET"}, name="get_all_recipies" )
      */
-  public function home(){
-     // $resp = new Response( '<h1>Hello World</h1>');
-      return $this ->json(['message'=>'How are you']);
-  }
+    public function recipies(Request $request): Response
+    {
+        $rootPath = $this->getParameter('kernel.project_dir');
+        $res = file_get_contents($rootPath . '/resources/recipes.json');
+        $resjson = json_decode($res, true);
+        return $this->json($resjson);
+    }
+
+    /**
+     * @Route("/recipies/{recipieId}",methods={"GET"}, name="get_recipies_by_id" )
+     */
+    public function recipieById(Request $request, $recipieId, LoggerInterface $logger): Response
+    {
+        $rootPath = $this->getParameter('kernel.project_dir');
+        $res = file_get_contents($rootPath . '/resources/recipes.json');
+        $resjson = json_decode($res, true);
+        $response = '';
+        foreach ($resjson['recipes'] as $key => $jsons) { // This will search in the 2 jsons
+            $logger->info($key);
+            if ($jsons['id'] == $recipieId) {
+                $response = $jsons;
+                break;
+            }
+        }
+        return $this->json($response);
+    }
+
+
 }
